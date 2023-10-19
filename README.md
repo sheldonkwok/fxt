119ff7e4a42070fb3da0a91b18d2efff60b09203
Sheldon Kwok <sheldon@secondspectrum.com>
2023-10-19 11:59:54 -0700

    feat: Fix twitter links

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# vxtbot
+# fxt 
---
+++ bot.ts
@@ -0,0 +1,41 @@
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+function parseUrl(urlStr: string): URL | undefined {
+  try {
+    return new URL(urlStr);
+  } catch {
+    return;
+  }
+}
+
+function fixTwitter(url: URL): string | undefined {
+  const { hostname } = url;
+  if (hostname !== "twitter.com" && hostname !== "x.com") return;
+
+  url.hostname = "fxtwitter.com";
+  return url.href;
+}
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on(Events.MessageCreate, (msg) => {
+  const { content } = msg;
+
+  const parsed = parseUrl(content);
+  if (!parsed) return;
+
+  const newUrl = fixTwitter(parsed);
+  if (!newUrl) return;
+
+  msg.reply(newUrl);
+});
+
+client.once(Events.ClientReady, (c) => console.log(`Ready! Logged in as ${c.user.tag}`));
+client.login(process.env.DISCORD_TOKEN);
