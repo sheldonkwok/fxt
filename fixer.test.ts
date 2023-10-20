9966fa3e88807116b79f213ddedfd64552ae2302
Sheldon Kwok <sheldon@secondspectrum.com>
2023-10-20 16:02:25 -0700

    feat: Handle spoilers

--- fixer.test.ts
+++ fixer.test.ts
@@ -3,6 +3,7 @@ import { fixMsg } from "./fixer";
 
 const TEST_URL = "https://twitter.com/ProZD/status/1714895861208293467";
 const FIXED_URL = "https://fxtwitter.com/ProZD/status/1714895861208293467";
+const FIXED_SPOILER = `||${FIXED_URL}||`;
 
 test("pass through normal text", () => {
   const fixed = fixMsg(`this
@@ -36,4 +37,23 @@ test("fix multiple urls", () => {
 
   const fixed = fixMsg(content);
   expect(fixed).toEqual([FIXED_URL, FIXED_URL]);
+});
+
+test("handle spoilers", () => {
+  const content = `shhhh ||${TEST_URL}||`;
+  const fixed = fixMsg(content);
+  expect(fixed).toEqual([FIXED_SPOILER]);
+});
+
+test("handle multiple spoilers", () => {
+  const content = `shhhh ||${TEST_URL}||
+  shh||${TEST_URL}|| `;
+  const fixed = fixMsg(content);
+  expect(fixed).toEqual([FIXED_SPOILER, FIXED_SPOILER]);
+});
+
+test("handle broken spoilers", () => {
+  const content = `shhhh ||secret|| ${TEST_URL}||`;
+  const fixed = fixMsg(content);
+  expect(fixed).toEqual([FIXED_URL]);
 });
--- fixer.ts
+++ fixer.ts
@@ -1,10 +1,50 @@
 export function fixMsg(content: string): string[] {
-  const matches = content.matchAll(/https:\/\/(twitter|x).com\/\w+\/status\/\d+/g);
+  const matches = Array.from(content.matchAll(/https:\/\/(twitter|x).com\/\w+\/status\/\d+/g));
   const fixes: string[] = [];
 
+  if (matches.length === 0) return fixes;
+
+  const spoilers = getSpoilers(content);
+
   for (const match of matches) {
-    fixes.push(match[0].replace(/(twitter|x).com/, "fxtwitter.com"));
+    const index = match.index;
+    if (index === undefined) continue;
+
+    let fix = match[0].replace(/(twitter|x).com/, "fxtwitter.com");
+
+    for (const [start, end] of spoilers) {
+      if (index >= start && index < end) fix = `||${fix}||`;
+    }
+
+    fixes.push(fix);
   }
 
   return fixes;
+}
+
+const SPOILER = "||";
+
+function getSpoilers(content: string): number[][] {
+  const indexes: number[] = [];
+  let start = 0;
+
+  while (start < content.length) {
+    const index = content.indexOf(SPOILER, start);
+    if (index === -1) break;
+
+    indexes.push(index);
+    start = index + 2;
+  }
+
+  const pairs: number[][] = [];
+  if (indexes.length < 2) return pairs;
+
+  for (let i = 0; i < indexes.length; i += 2) {
+    const start = indexes[i];
+    const end = indexes[i + 1];
+
+    pairs.push([start, end]);
+  }
+
+  return pairs;
 }
